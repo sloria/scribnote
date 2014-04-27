31483bc1d09e1d5c5433ee90d0cc19f77c99f9ba
Steven Loria <sloria1@gmail.com>
2014-04-27 00:11:42 -0400

    Implement ModelResource#put and add ability to add links to any respons

--- server/app/meta/api.py
+++ server/app/meta/api.py
@@ -4,7 +4,7 @@ marshalling, etc.
 """
 import httplib as http
 
-from flask import request
+from flask import request, url_for
 from flask.ext.classy import FlaskView
 from flask.ext.api.exceptions import NotFound, APIException
 from webargs import core
@@ -30,7 +30,10 @@ class FlaskAPIParser(FlaskParser):
     }
 
     def parse_data(self, req, name, arg):
-        return core.get_value(req.data, name, arg.multiple)
+        try:
+            return core.get_value(req.data, name, arg.multiple)
+        except AttributeError:
+            return None
 
     def handle_error(self, error):
         raise BadRequestError(str(error))
@@ -39,52 +42,95 @@ reqparser = FlaskAPIParser()
 use_kwargs = reqparser.use_kwargs
 use_args = reqparser.use_args
 
+class APIView(FlaskView):
+    """An abstract APIView."""
 
-class ModelResource(FlaskView):
-    """Implements generic HTTP-based CRUD for a given model.
-    Must define ``MODEL`` and ``SERIALIZER`` class variables.
-    """
+    ARGS = None
     MODEL = None
     SERIALIZER = None
     NAME = None
+    BLUEPRINT = None
+
+    def parse_request(self):
+        if self.ARGS:
+            args = reqparser.parse(self.ARGS, request, targets=('data', 'json'))
+        else:
+            args = {}
+        return args
+
+    def get_name(self):
+        return getattr(self, 'NAME', 'object')
+
+    def get_links(self):
+        return {}
+
+    def post_links(self):
+        return {}
+
+    def put_links(self):
+        return {}
+
+
+class ModelResource(APIView):
+    """Implements generic HTTP-based CRUD for a given model.
+    Must define ``MODEL`` and ``SERIALIZER`` class variables.
+    """
 
     def get(self, id):
         """Return a single instance."""
         name = getattr(self, 'NAME', 'Resource')
         obj = api_get_or_404(self.MODEL, id,
             error_msg="{name} not found".format(name=name))
-        return {
+        res = {
             'result': self.SERIALIZER(obj, strict=True).data,
-        }, http.OK
+        }
+        res.update(self.get_links())
+        return res, http.OK
+
+    def put(self, id):
+        obj = api_get_or_404(self.MODEL, id)
+        attrs = self.parse_request()
+        obj.update(**attrs)
+        obj.save()
+        res = {
+            'result': self.SERIALIZER(obj, strict=True).data,
+            'message': 'Updated {0}'.format(self.get_name())
+        }
+        res.update({
+            '_links': self.put_links(),
+        })
+        return res, http.OK
 
 
-class ModelListResource(FlaskView):
+class ModelListResource(APIView):
     """Implements generic HTTP-based collection CRUD for a given model."""
 
-    MODEL = None
-    SERIALIZER = None
-    NAME = None
-    ARGS = None
 
     def get(self):
         """Return a list of the requested objects."""
         objects = self.MODEL.query.all()
-        return {
-            'result': self.SERIALIZER(objects, many=True).data
-        }, http.OK
+        assert self.BLUEPRINT is not None, 'Must define BLUEPRINT'
+        res = {
+            'result': self.SERIALIZER(objects, many=True).data,
+        }
+        res.update({
+            '_links': self.get_links(),
+        })
+        return res, http.OK
 
     def post(self):
         """Create a new instance of the resource."""
-        if self.ARGS:
-            args = reqparser.parse(self.ARGS, request, targets=('data', 'json'))
-        else:
-            args = {}
+        args = self.parse_request()
         new_obj = self.MODEL.create(**args)
-        name = getattr(self, 'NAME', 'object')
-        return {
+        name = self.get_name()
+        res = {
             'result': self.SERIALIZER(new_obj, strict=True).data,
             'message': 'Successfully created new {0}'.format(name)
-        }, http.CREATED
+        }
+        res.update({
+            '_links': self.post_links(),
+        })
+        return res, http.CREATED
 
 
 def api_get_or_404(model, id, error_msg=None):
@@ -100,6 +146,3 @@ def api_get_or_404(model, id, error_msg=None):
 def register_class_views(view_classes, app, route_base=None, route_prefix=None):
     for each in view_classes:
         each.register(app, route_base=route_base, route_prefix=route_prefix)
-
-def register_api_views(view_classes, app, route_prefix='/api/'):
-    register_class_views(view_classes, app, route_prefix=route_prefix)
