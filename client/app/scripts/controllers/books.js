943de5eb9dc21eab26544a8c003b75d8b6119856
Steven Loria <sloria1@gmail.com>
2014-04-28 00:59:37 -0400

    Create book post stub

--- client/app/scripts/controllers/books.js
+++ client/app/scripts/controllers/books.js
@@ -4,9 +4,29 @@ var app = angular.module('appApp');
 
 app.controller('BooksCtrl', function ($scope, Books) {
   var self = this;
+
+  self.addForm = {
+    active: false,
+    first: null,
+    last: null,
+    title: null,
+    submit: function() {
+      var payload = {
+        title: this.title,
+        author_first: this.first,
+        author_last: this.last
+      };
+      console.log(payload);
+    }
+  };
+
   Books.query(function(response) {
     self.books = response.result;
   });
+
+  self.addBook = function() {
+    self.addForm.active = true;
+  };
   // Namespace controller
   $scope.BooksCtrl = self;
 });
