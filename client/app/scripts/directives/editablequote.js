835aa8910a7e3984df8d368b0d0c74bfb6a7d768
Steven Loria <sloria1@gmail.com>
2014-05-03 01:27:58 -0400

    Add editable-quote directive

---
+++ client/app/scripts/directives/editablequote.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var app = angular.module('appApp');
+
+app.directive('editableQuote', function ($hotkey) {
+  return {
+    templateUrl: 'scripts/directives/editable-quote.html',
+    transclude: true,
+    restrict: 'E',
+    scope: {
+      eqText: '@',
+      eqRows: '@',
+      eqCols: '@',
+      eqSubmit: '&',
+      eqCiteAuthor: '@',
+      eqCiteTitle: '@',
+      eqPlaceholder : '@'
+    },
+    link: function postLink(scope, element, attrs) {
+      var oldText = scope.eqText;
+
+      scope.EDITING = 'e';
+      scope.VIEWING = 'v';
+      scope.MODE = scope.VIEWING;
+
+      // Enable text editing
+      scope.activate = function() {
+        scope.MODE = scope.EDITING;
+      };
+
+      // Disable text editing
+      scope.deactivate = function() {
+        scope.MODE = scope.VIEWING;
+      };
+
+      scope.submit = function() {
+        scope.eqSubmit({newText: scope.eqText, oldText: oldText});
+        scope.MODE = scope.VIEWING;
+      };
+    }
+  };
+});
