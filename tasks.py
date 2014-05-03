01c5e6cc15045742537cdb92e85d8df20e383ee3
Steven Loria <sloria1@gmail.com>
2014-05-03 09:48:13 -0400

    Remove old app.py

--- tasks.py
+++ tasks.py
@@ -2,20 +2,12 @@
 """The root invoke task namespace. Allows both client and server app
 tasks to be run from the project root.
 """
-from invoke import Collection, task
+from invoke import Collection
 
 import client.tasks
 import server.tasks
-import app
-
-
-@task
-def serve():
-    app.app.run(debug=True)
-
 
 namespace = Collection()
-# Namespace client and server tasks to c and s, respectively
+# Namespace client and server tasks to 'c' and 's', respectively
 namespace.add_collection(client.tasks, 'c')
 namespace.add_collection(server.tasks, 's')
-namespace.add_task(serve)
