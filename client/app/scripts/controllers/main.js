d4b875d886ff91bb9d9f583dbb9bdb23ab75b570
Steven Loria <sloria1@gmail.com>
2014-05-07 00:53:06 -0500

    Finish register/login forms and stub controller

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,10 +1,32 @@
 'use strict';
 
-angular.module('appApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+var app = angular.module('appApp');
+
+app.controller('MainCtrl', ['$scope', function ($s) {
+  $s.REGISTER = 'register';
+  $s.LOGIN = 'login';
+  $s.FORM = $s.REGISTER;
+
+  $s.newUser = {
+    first: '',
+    last: '',
+    email: '',
+    password: ''
+  };
+
+  $s.user = {
+    email: '',
+    password: ''
+  };
+
+  $s.submitRegister = function() {
+    console.log('submitting...');
+    console.log($s.newUser);
+  };
+
+  $s.submitLogin = function() {
+    console.log('submitting login..');
+    console.log($s.user);
+  };
+
+}]);
