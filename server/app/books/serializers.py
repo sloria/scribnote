378a5e581fb841a6f3664664a7d5cb73443e3686
sloria <sloria1@gmail.com>
2014-05-31 21:57:44 -0400

    Use marshmallow's new two-way nesting feature

--- server/app/books/serializers.py
+++ server/app/books/serializers.py
@@ -2,26 +2,10 @@
 #
 from ..extensions import ma
 
-# Serializers
-class BaseBookMarshal(ma.Serializer):
-    created = ma.DateTime(attribute='date_created')
-
-    _links = ma.Hyperlinks({
-        'self': ma.AbsoluteURL('books.BookDetail:get', id='<id>'),
-        'collection': ma.AbsoluteURL('books.BookList:get'),
-        'notes': ma.AbsoluteURL('books.BookDetail:notes', book_id='<id>')
-    })
-
-    class Meta:
-        dateformat = 'iso'
-        additional = ('id', 'title', 'isbn')
-
-
 class AuthorMarshal(ma.Serializer):
     created = ma.DateTime(attribute='date_created')
-    books = ma.Nested(BaseBookMarshal, many=True)
+    books = ma.Nested('BookMarshal', many=True, only=('id', 'title'))
 
-    # Implement HATEOAS
     _links = ma.Hyperlinks({
         'self': ma.AbsoluteURL('books.AuthorDetail:get', id='<id>'),
         'update': ma.AbsoluteURL('books.AuthorDetail:put', id='<id>'),
@@ -32,9 +16,19 @@ class AuthorMarshal(ma.Serializer):
         dateformat = 'iso'
         additional = ('id', 'first', 'last')
 
-class BookMarshal(BaseBookMarshal):
+class BookMarshal(ma.Serializer):
+    created = ma.DateTime(attribute='date_created')
     author = ma.Nested(AuthorMarshal, exclude=('books', ), allow_null=True)
 
+    _links = ma.Hyperlinks({
+        'self': ma.AbsoluteURL('books.BookDetail:get', id='<id>'),
+        'collection': ma.AbsoluteURL('books.BookList:get'),
+        'notes': ma.AbsoluteURL('books.BookDetail:notes', book_id='<id>')
+    })
+
+    class Meta:
+        dateformat = 'iso'
+        additional = ('id', 'title', 'isbn')
 
 serialize_author = AuthorMarshal.factory(strict=True)
 serialize_book = BookMarshal.factory(strict=True)
